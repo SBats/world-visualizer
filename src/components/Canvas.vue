a5fc8ba1cda307b4a28435fad146bba0f98c93bd
SBats <simon.bats12@gmail.com>
2020-06-15 23:01:29 -0400

    Setup use of Konva

---
+++ src/components/Canvas.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-stage :config="stageConfig">
+    <v-layer>
+      <v-circle :config="entityConfig"></v-circle>
+    </v-layer>
+  </v-stage>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Canvas extends Vue {
+  private stageConfig = {
+    width: 200,
+    height: 200
+  };
+
+  private entityConfig = {
+    x: 100,
+    y: 100,
+    radius: 70,
+    fill: "red",
+    stroke: "black",
+    strokeWidth: 4
+  };
+}
+</script>
