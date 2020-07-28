6f2e0857a87efed8fda9d1287b48f90cdd7a1061
SBats <simon.bats12@gmail.com>
2020-07-28 08:53:58 -0400

    Use force graph to represent graph

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,39 +1,59 @@
 <template>
-  <v-stage :config="stageConfig">
+  <!-- <v-stage :config="stageConfig">
     <v-layer>
       <v-circle :config="entityConfig"></v-circle>
     </v-layer>
-  </v-stage>
+  </v-stage>-->
+  <div id="graph-container"></div>
 </template>
+
+<style>
+#graph-container {
+  width: 100vw;
+  height: 50vh;
+}
+</style>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Getter } from "vuex-class";
-import { Graph } from "ug-ts";
+import ForceGraph, { ForceGraphInstance, GraphData } from "force-graph";
 
 @Component
 export default class Canvas extends Vue {
-  @Getter("graph") graph!: Graph;
+  @Getter("formattedGraph") graph!: GraphData;
+  forceGraph?: ForceGraphInstance;
+  // private stageConfig = {
+  //   width: 200,
+  //   height: 200,
+  // };
 
-  private stageConfig = {
-    width: 200,
-    height: 200,
-  };
+  // private entityConfig = {
+  //   x: 40,
+  //   y: 40,
+  //   radius: 20,
+  //   fill: "red",
+  //   stroke: "black",
+  //   strokeWidth: 1,
+  // };
 
-  private entityConfig = {
-    x: 40,
-    y: 40,
-    radius: 20,
-    fill: "red",
-    stroke: "black",
-    strokeWidth: 1,
-  };
-
-  created() {
+  mounted() {
     if (this.graph) {
       console.log(this.graph);
+      this._drawGraph(this.graph);
     }
+  }
+
+  private _drawGraph(graph: GraphData) {
+    const container = document.getElementById("graph-container");
+    if (!container) return;
+    this.forceGraph = ForceGraph();
+    this.forceGraph(container)
+      .width(container.clientWidth)
+      .height(container.clientHeight)
+      .linkDirectionalParticles(2)
+      .graphData(graph);
   }
 }
 </script>
